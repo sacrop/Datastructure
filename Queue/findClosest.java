8234ba17995be7fd2386f69bd6d26468ec3b1ca8
sidharth ram <acnellithave@gmail.com>
2023-12-16 17:01:41 +0530

    recursion sorting

---
+++ Queue/findClosest.java
@@ -0,0 +1,6 @@
+public class findClosest {
+    public static void main(String[] args) {
+        int[] arr={-4,-1,-2,3,4,-5};
+        
+    }
+}
---
+++ Stack/BubbleSort.java
@@ -0,0 +1,15 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr={1,10,100,0}
+    }
+    static void BubbleSort(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length-i;i++){
+                if(arr[j]>arr[j-1]){
+                    
+                }
+            }
+
+        }
+    }
+}
