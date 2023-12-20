6a8d41671309374a1dd1d6277322afabcfe17a52
sidharth ram <acnellithave@gmail.com>
2023-12-20 18:06:33 +0530

    binary search tree

--- tree/BinarySearchTree.java
+++ tree/BinarySearchTree.java
@@ -2,8 +2,12 @@ public class BinarySearchTree {
     public static void main(String[] args) {
         BinarySearchTree bst=new BinarySearchTree();
         bst.insertNode(10);
+        bst.insertNode(20);
+        bst.insertNode(33);
+        bst.insertNode(34);
         bst.display();
-
+        bst.delete(33);
+        bst.display();
     }
     class Node{
         private int val;
@@ -40,8 +44,43 @@ public class BinarySearchTree {
             return;
         }
         System.out.println(message+node.val);
-        display(node.left,"left child of "+node.val);
-        display(node.right,"right child of "+node.val);
+        display(node.left,"left child of "+node.val+" is :");
+        display(node.right,"right child of "+node.val+" is :");
+    }
+    public void delete(int val){
+        root=delete(root,val);
+    }
+    private Node delete(Node node,int val){
+        if(node==null){
+            return null;
+        }
+        if(val>node.val){
+            node.right=delete(node.right,val);
+        }
+        else if(val<node.val){
+            node.left=delete(node.left,val);
+        }
+        else{
+            if(node.left==null){
+                return node.right;
+            }
+            else if(node.right==null){
+                return node.left;
+            }
+            else{
+                node.val=Minval(node.right);
+                node.right=delete(node.right,node.val);
+            }
+        }
+        return node;
+    }
+    private int Minval(Node node){
+        int min=node.val;
+        while(node.left!=null){
+            min=node.left.val;
+            node=node.left;
+        }
+        return min;
     }
     
 }
